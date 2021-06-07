1c99d253e9739028014c9c92b69900985989a4bc
Tito <ebenirot@gmail.com>
2021-06-07 12:40:23 +0100

    --- Auto-Git Commit ---

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -1,1 +1,12 @@
 const nodeMailer = require("nodemailer");
+
+exports.sendMail = async (option) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SM,
+        port: 587,
+        secure: false, // upgrade later with STARTTLS
+        auth: {
+        user: "username",
+        pass: "password"
+    })
+}
